a26f61a38e739320e653deb5c472a3d062361dad
Alexander Templeton <azide0x37@users.noreply.github.com>
2015-04-13 03:37:12 -0500

    scraping works, ready to load database

--- scraper.py
+++ scraper.py
@@ -1,22 +1,56 @@
 #modocDB/scraper.py
-#Scrapes the offender web search library for offender data
-import csv
-import lxml #use this to speed up parsing for large datasets
-import pymongo 
+"""
+Scrapes the Missouri Department of Corrections 
+Offender web search site for offender data.
+
+Stores in MongoDB collection
+"""
+#import lxml #TODO: implement this to speed up parsing
+#import pymongo #get this working
 import requests
-import BeautifulSoup as bs4
+from bs4 import BeautifulSoup
 
 class docScraper:
-	def __init__(self, url = "", _offenders = 1300000):
-		self._url = 
-		self._offenders = _offenders
-		self._client = pymongo.MongoClient("localhost", 27017)
-		self._db_offenders = self._client.db_offenders
-	
-	def _parse(self):
-		##beautiful soup parsing
-	def pull(self):
-		result = [self._db_offenders.insert_one(self._parse(requests.post(self._url + "docId=", docId)) for docId in xrange(self._offenders)]
+    def __init__(self, url = "https://web.mo.gov/doc/offSearchWeb/searchOffender.do", _offenders = 1230000):
+        self._url = url
+        self._offenders = _offenders
+        #self._client = pymongo.MongoClient("localhost", 27017)
+        #self._db_offenders = self._client.db_offenders
 
+    def _parse(self, _rawHTML):
+        ##beautiful soup parsing
+        _souped = BeautifulSoup(_rawHTML)
+        _table = _souped.find('table', { "class" : "displayTable" })
+        try:
+            _cols = _table.findAll('tr')
+            _data = {}        
+            for tr in _cols:
+                td = tr.findAll('td')
+                try:                
+                    results = ["".join(_.find(text=True)).strip() for _ in td if _ != '']
+                except(TypeError):
+                    pass
+
+                if (len(results) == 3):
+                    results.pop(0)
+                
+                try:
+                    _key, _value = results[0:2]
+                    _data[_key] = _value
+                except(ValueError):
+                    pass
+                
+            print _data
+                #_key = "".join(td.find(text=True))
+                #_value = "".join(td.findNext(text=True))
+                #_data[_key] = _value
+        
+        except(AttributeError):
+            pass
+    
+    def pull(self):
+        dataset = [self._parse(requests.get(self._url + "?docId=" + str(docId)).text) for docId in xrange(1229000, self._offenders)]
+                  
+         
 dataSet = docScraper()
 dataSet.pull()
