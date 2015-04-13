7cd37d9b2ef82218214a251638815b8608ac9123
Alexander Templeton <azide0x37@users.noreply.github.com>
2015-04-12 22:44:14 -0500

    added parse(), setup mongoDB writes

--- scraper.py
+++ scraper.py
@@ -12,14 +12,11 @@ class docScraper:
 		self._offenders = _offenders
 		self._client = pymongo.MongoClient("localhost", 27017)
 		self._db_offenders = self._client.db_offenders
-		
-	def pull():
-		dataSet = (requests.post(self._url + "docId=", docId for docId in xrange(self._offenders))
-		
-		try:
-			#TODO: check if already in dataset and discard
-			for n in xrange(self._offenders):
-				self._db_offenders.write = mongoDBwriter.row_add(BeautifulSoup.parse(dataSet.next()))
+	
+	def _parse(self):
+		##beautiful soup parsing
+	def pull(self):
+		result = [self._db_offenders.insert_one(self._parse(requests.post(self._url + "docId=", docId)) for docId in xrange(self._offenders)]
 
 dataSet = docScraper()
 dataSet.pull()
