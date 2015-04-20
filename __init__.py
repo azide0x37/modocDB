5ed133ab0d44483b3cbfd35f73676a2b70213df2
Alexander Templeton <blakmatr@gmail.com>
2015-04-20 07:39:34 +0000

    wooooo

--- __init__.py
+++ __init__.py
@@ -4,10 +4,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    return render_template('main.html')
+def homepage(name=None):
+    return render_template('main.html', name=name)
 
-@app.route('/test')
+@app.route('/test/')
 def testpage():
     return "Test page, get rekt"
 
