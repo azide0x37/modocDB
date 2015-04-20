edbdc96a171d1061e63bc11d407084e3c4b1265f
Alexander Templeton <blakmatr@gmail.com>
2015-04-20 16:32:57 +0000

    updated bootstrap

--- __init__.py
+++ __init__.py
@@ -6,12 +6,36 @@ app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
 @app.route('/')
-def homepage(table_data = None):
-    return render_template('main.html', table_data='http://wenzhixin.net.cn/p/bootstrap-table/docs/data1.json')
+def homepage():
 
-@app.route('/test/')
-def testpage():
-    return "Test page, get rekt"
+    title = "Epic Tutorials"
+    paragraph = ["wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!","wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!"]
+
+    try:
+        return render_template("main.html", title = title, paragraph=paragraph)
+    except Exception, e:
+        return str(e)
+
+@app.route('/about')
+def aboutpage():
+
+    title = "About this site"
+    paragraph = ["blah blah blah memememememmeme blah blah memememe"]
+
+    pageType = 'about'
+
+    return render_template("main.html", title=title, paragraph=paragraph, pageType=pageType)
+
+
+@app.route('/about/contact')
+def contactPage():
+
+    title = "About this site"
+    paragraph = ["blah blah blah memememememmeme blah blah memememe"]
+
+    pageType = 'about'
+
+    return render_template("main.html", title=title, paragraph=paragraph, pageType=pageType)
 
 if __name__ == "__main__":
     app.run()
