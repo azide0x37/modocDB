858d787b98bc0915e77f233c42633ef9ada323af
Alexander Templeton <blakmatr@gmail.com>
2015-04-20 15:54:43 +0000

    heroku and origin match now

--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 web: gunicorn __init__:app --log-file=-
-=======
-web: python app.py
->>>>>>> 757cadd3d02be56cb11b56e79499d1fb77f2390e
--- __init__.py
+++ __init__.py
@@ -16,15 +16,3 @@ def testpage():
 
 if __name__ == "__main__":
     app.run()
-=======
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def homepage():
-    return render_template("main.html")
-
-if __name__ == "__main__":
-    app.run()
->>>>>>> 757cadd3d02be56cb11b56e79499d1fb77f2390e
