ee88845586c8a2951c26578ca2ca5cde13ee8ad4
Alexander Templeton <azide0x37@users.noreply.github.com>
2015-04-23 13:04:13 -0500

    added errorhandler @ afterrequest

--- __init__.py
+++ __init__.py
@@ -58,5 +58,30 @@ def todoPage():
 
     return render_template("todo_list.html", title=title, paragraph=paragraph, pageType=pageType)
 
+###
+# The functions below should be applicable to all Flask apps.
+###
+
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+    """Send your static text file."""
+    file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
